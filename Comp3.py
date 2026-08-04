9cb26a9bab68fa3293b28a732fd03126e43b5ef5
Louis <louis6773@gmail.com>
2026-08-05 10:11:18 +1200

    Simulating purchasing the upgrade
    Included upgrade properties and on to testing the upgrade

--- Comp3.py
+++ Comp3.py
@@ -3,32 +3,32 @@
 Component 3 - Upgrade class
 """
  
-# the upgrade class stores the data for a single upgrade
+# the Upgrade class stores the data for a single upgrade
 class Upgrade:
    def __init__(self, name, cost, power_increase):
-       # name of the upgrade shown
+       # name of the upgrade shown on the button
        self.name = name
  
-       # how much currency it costs
+       # how much currency it costs to buy
        self.cost = cost
  
        # how much click power it adds when bought
        self.power_increase = power_increase
  
-       # trtakcs if player already owns this upgrade
+       # tracks whether the player already owns this upgrade
        self.owned = False
  
  
-
-
-
-
-
-
-
-
-
-
-
-
-
+# quick test to make sure the class works
+if __name__ == "__main__":
+   upgrade1 = Upgrade("Better Clicks", 10, 1)
+ 
+   print("Name:", upgrade1.name)
+   print("Cost:", upgrade1.cost)
+   print("Power increase:", upgrade1.power_increase)
+   print("Owned:", upgrade1.owned)
+ 
+   # simulate buying the upgrade
+   upgrade1.owned = True
+   print("Owned after purchase:", upgrade1.owned)
+ 
