5319385cfb6333cbeaedee24ab4476db37b74398
Louis <louis6773@gmail.com>
2026-08-04 14:39:19 +1200

    Setting up the Upgrade Class
    Set up the foundations and the main variables

---
+++ Comp3.py
@@ -0,0 +1,34 @@
+"""
+90196 Louis Fletcher
+Component 3 - Upgrade class
+"""
+ 
+# the upgrade class stores the data for a single upgrade
+class Upgrade:
+   def __init__(self, name, cost, power_increase):
+       # name of the upgrade shown
+       self.name = name
+ 
+       # how much currency it costs
+       self.cost = cost
+ 
+       # how much click power it adds when bought
+       self.power_increase = power_increase
+ 
+       # trtakcs if player already owns this upgrade
+       self.owned = False
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
