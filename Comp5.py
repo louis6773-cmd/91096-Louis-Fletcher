d2643b789ab1c519733d342757b92939b2ef0bf2
Louis <louis6773@gmail.com>
2026-08-12 10:07:15 +1200

    Added classes
    Added basic classes for component (player and upgrade)

--- Comp5.py
+++ Comp5.py
@@ -2,12 +2,33 @@
 90196 Louis Fletcher
 Component 5 - Buy Upgrade function
 """
+# stores all data on player’s progress
+class Player:
+    def __init__(self):
+        self.currency = 0
+        self.click_power = 1
+
+# stores all data on upgrades
+class Upgrade:
+    def __init__(self, name, cost, power_increase):
+        self.name = name
+        self.cost = cost
+        self.power_increase = power_increase
+        self.owned = False
 
 # checks whether the player can afford the upgrade
 def buy_upgrade(player, upgrade):
     if player.currency >= upgrade.cost:
-        print("Can afford this upgrade")
+        # subtract the cost from the player's currency
+        player.currency -= upgrade.cost
+        # apply upgrade's effect
+        player.click_power += upgrade.power_increase
+        upgrade.owned = True
+
+        print("Upgrade purchased")
     else:
-        print("cannot afford this upgrade")
-        
+        print("Cannot afford this upgrade")
+
+
+
 
